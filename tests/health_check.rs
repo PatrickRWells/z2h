21f95d9b6185d0adb6f3013bffb94e760da4a575
Patrick Wells <patrick.wells.95@gmail.com>
2024-02-23 10:39:13 -0800

    more checks

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,7 +24,7 @@ async fn subscribe_returns_200_for_valid_form() {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
 
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
 
@@ -42,7 +42,7 @@ async fn subscribe_returns_200_for_valid_form() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&connection)
+        .fetch_one(&mut connection)
         .await
         .expect("Failed to fetch saved subscription.");
 
