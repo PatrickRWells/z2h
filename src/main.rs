6a8331f30cdf7fb63f1bf7ca7b664202db43a7bd
Patrick Wells <patrick.wells.95@gmail.com>
2024-03-06 17:26:28 -0800

    Fix security issues

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use zero2prod::startup::run;
-use zero2prod::configuration::get_configuration;
+use sqlx::PgPool;
 use std::net::TcpListener;
+use secrecy::ExposeSecret;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // If RUST_LOG is not set, default to info
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
