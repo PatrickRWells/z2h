418b336b01624a7d1dccdb028e69a6b9ffc421dd
Patrick Wells <patrick.wells.95@gmail.com>
2024-03-06 17:29:34 -0800

    add edits

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,60 @@
+use actix_web::{web, HttpResponse, Responder};
+use sqlx::PgPool;
+use tracing::Instrument;
+use uuid;
+#[derive(serde::Deserialize)]
+pub(crate) struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, connection_pool),
+    fields(
+        email = %form.email,
+        name = %form.name
+    )
+)]
+pub(crate) async fn subscribe(
+    form: web::Form<FormData>,
+    connection_pool: web::Data<PgPool>,
+) -> impl Responder {
+    match insert_subscriber(&connection_pool, &form).await
+        {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool),
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        uuid::Uuid::new_v4(),
+        form.email,
+        form.name,
+        chrono::Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
